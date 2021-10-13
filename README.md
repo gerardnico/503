443bbd1639f381a0b6696cca4740540985b9c554
gerardnico <gerardnico@gmail.com>
2021-10-13 13:07:14 +0200

    Added favicon

--- README.md
+++ README.md
@@ -3,6 +3,14 @@
 As explained here:
 https://developers.google.com/search/docs/advanced/crawling/pause-online-business
 
+# Deploy
+
+```
+yarn global add vercel
+vercel login
+vercel 
+```
+
 
 # Usage
 https://github.com/juicyfx/vercel-php
--- api/index.php
+++ api/index.php
@@ -1,26 +1,46 @@
 <?php
 
+function serveStaticFile($resourcePath)
+{
+    if (file_exists($resourcePath)) {
+        http_response_code("200");
+        $extension = pathinfo($resourcePath, PATHINFO_EXTENSION);
+        if ($extension === "svg") {
+            // Text based
+            header("Content-type: image/svg+xml");
+            echo file_get_contents($resourcePath);
+        } else {
+            // Binary based
+            $mimeContentType = mime_content_type($resourcePath);
+            if ($mimeContentType !== false) {
+                header("Content-type: $mimeContentType");
+            }
+            $fp = fopen($resourcePath, 'rb');
+            fpassthru($fp);
+        }
+        exit;
+    } else {
+        http_response_code("404");
+        echo "This file does not exist";
+    }
+}
+
 $scriptFileName = $_SERVER["SCRIPT_NAME"];
 
 $firstDirectory = "";
 $fileParts = explode('/', $scriptFileName);
 /** The first element should be blank */
-if(sizeof($fileParts)>=2){
+if (sizeof($fileParts) >= 2) {
     $firstDirectory = $fileParts[1];
 }
 switch ($firstDirectory) {
     case "resources":
-        $resourcePath = __DIR__ . "/../".$scriptFileName;
-        if(file_exists($resourcePath)){
-            http_response_code("200");
-            header("Content-type: image/svg+xml");
-            $fp = fopen($resourcePath, 'rb');
-            fpassthru($fp);
-            exit;
-        } else {
-            http_response_code("404");
-            echo "This file does not exist";
-        }
+        $resourcePath = __DIR__ . "/../" . $scriptFileName;
+        serveStaticFile($resourcePath);
+        break;
+    case "favicon.ico":
+        $resourcePath = __DIR__ . "/../resources/favicon.ico";
+        serveStaticFile($resourcePath);
         break;
     default:
         http_response_code("503");
